978b17750461891e9e8d5b631fab45e9dec8ad36
chiefalbemarle <94005050+chiefalbemarle@users.noreply.github.com>
2021-11-09 15:38:58 -0600

    Add files via upload

---
+++ factinternetsales_cleanup.sql
@@ -0,0 +1,32 @@
+--factinternetsales
+--tabbed out unnecessary columns
+--cut rows to only 2019 and beyond
+SELECT [ProductKey]
+      ,[OrderDateKey]
+      ,[DueDateKey]
+      ,[ShipDateKey]
+      ,[CustomerKey]
+      --,[PromotionKey]
+      --,[CurrencyKey]
+      --,[SalesTerritoryKey]
+      ,[SalesOrderNumber]
+      --,[SalesOrderLineNumber]
+      --,[RevisionNumber]
+      --,[OrderQuantity]
+      --,[UnitPrice]
+      --,[ExtendedAmount]
+      --,[UnitPriceDiscountPct]
+      --,[DiscountAmount]
+      --,[ProductStandardCost]
+      --,[TotalProductCost]
+      ,[SalesAmount]
+      --,[TaxAmt]
+      --,[Freight]
+      --,[CarrierTrackingNumber]
+      --,[CustomerPONumber]
+      --,[OrderDate]
+      --,[DueDate]
+      --,[ShipDate]
+  FROM [AdventureWorksDW2019].[dbo].[FactInternetSales]
+  where
+  left ([OrderDateKey],4)>=year(getdate())-2
